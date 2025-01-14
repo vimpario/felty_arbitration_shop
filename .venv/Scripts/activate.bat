9d21839b6f0b73cad6bad11a667851bad495ff76
vimpario <koz.viktor2020@gmail.com>
2025-01-14 08:21:10 +0300

    Docker container

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Victor\Desktop\workspace\felty_arbitration_shop\.venv
+set VIRTUAL_ENV=C:\Users\sindr\OneDrive\Рабочий стол\workspace\felty_arbitration_shop\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
